108ab5ce231c75f0c487e2bfbd8612af8e11f353
Michael Williams <manicdesigner@gmail.com>
2020-01-11 12:28:14 -0500

    worked on knexfile

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/tasks.db3',
+    },
+  },
+  migrations: {
+    directory: './migrations',
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      // runs after a connection is made to the sqlite engine
+      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+    },
+  },
+};
