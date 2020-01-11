19b42d44bd39901be27495844b8a5cc35edc8fc7
Michael Williams <manicdesigner@gmail.com>
2020-01-11 12:37:58 -0500

    built out projects table

--- migrations/20200111123226_inital.js
+++ migrations/20200111123226_inital.js
@@ -1,5 +1,16 @@
 exports.up = async function(knex) {
-  await knex.schema.createTable('projects', table => {});
+  await knex.schema.createTable('projects', table => {
+    table.increments('id');
+    table
+      .string('project_name', 160)
+      .notNullable()
+      .unique();
+    table.text('project_description');
+    table
+      .boolean('project_completed')
+      .notNullable()
+      .defaultTo(false);
+  });
 
   await knex.schema.createTable('resources', table => {});
 
