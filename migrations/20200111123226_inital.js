675514a919ecd5fdc506f3a6fae0a96a7853a8e5
Michael Williams <manicdesigner@gmail.com>
2020-01-13 18:54:41 -0500

    rebuilt tables

--- migrations/20200111123226_inital.js
+++ migrations/20200111123226_inital.js
@@ -12,21 +12,6 @@ exports.up = async function(knex) {
       .defaultTo(false);
   });
 
-  await knex.schema.createTable('resources', table => {
-    table.increments('id');
-    table
-      .string('resource_name')
-      .notNullable()
-      .unique();
-    table.text('resource_description');
-    table
-      .integer('projects_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('projects');
-  });
-
   await knex.schema.createTable('tasks', table => {
     table.increments('id');
     table.string('task_description', 250).notNullable();
@@ -42,10 +27,33 @@ exports.up = async function(knex) {
       .references('id')
       .inTable('projects');
   });
-};
 
+  await knex.schema.createTable('resources', table => {
+    table.increments('id');
+    table
+      .string('resource_name')
+      .notNullable()
+      .unique();
+    table.text('resource_description');
+  });
+
+  await knex.schema.createTable('project_resources', table => {
+    table
+      .integer('projects_id')
+      .unsigned()
+      .references('id')
+      .inTable('projects');
+    table
+      .integer('resources_id')
+      .unsigned()
+      .references('id')
+      .inTable('resources');
+    table.primary(['projects_id', 'resources_id']);
+  });
+};
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists('tasks');
+  await knex.schema.dropTableIfExists('projects_resources');
   await knex.schema.dropTableIfExists('resources');
+  await knex.schema.dropTableIfExists('tasks');
   await knex.schema.dropTableIfExists('projects');
 };
