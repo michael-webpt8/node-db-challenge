f75ff42e00c7dca8e5ad56dcdc91500d0f332130
Michael Williams <manicdesigner@gmail.com>
2020-01-13 19:01:07 -0500

    add seeds for building ideas in sql browser

--- seeds/003_resources.js
+++ seeds/003_resources.js
@@ -3,17 +3,14 @@ exports.seed = async function(knex) {
     {
       resource_name: 'a resource',
       resource_description: 'something descriptive',
-      projects_id: 1,
     },
     {
       resource_name: 'a resource for 2',
       resource_description: 'something descriptive again',
-      projects_id: 2,
     },
     {
       resource_name: 'a 111 resource',
       resource_description: 'something descriptive 111',
-      projects_id: 3,
     },
   ]);
 };
